e672537becbca5aa5de59cdd0e1d8090c29043a1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-23 09:25:46 -0500

    finished

--- cypress/e2e/dropdown.cy.js
+++ cypress/e2e/dropdown.cy.js
@@ -1,31 +1,38 @@
-// enables intelligent code completion for Cypress commands
-// https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 it('picks a country using the dropdown', () => {
   cy.visit('index.html')
   cy.contains('th', 'Country').invoke('index').should('equal', 7)
   cy.get('tbody tr')
-    .should('have.length.greaterThan', 3)
+    .should('have.length.above', 3)
     .eq(1)
     .find('td')
     .eq(7)
     .find('img')
-    .should('have.attr', 'title' /*, 'Great Britain'*/)
+    .should('have.attr', 'title')
     .then((country) => {
-      return country === 'Great Britain' ? 'France' : 'Great Britain'
+      return country === 'Brazil' ? 'Great Britain' : 'Brazil'
     })
     .as('country')
-  cy.get('tbody tr').eq(1).find('td').eq(7).find('img').click()
-  cy.get('td#grid_active_cell .k-picker').click()
-  cy.get('ul[role=listbox]')
-    .should('be.visible')
-    .find('li')
-    .should('have.length.greaterThan', 2)
+  cy.get('tbody tr')
+    .should('have.length.above', 3)
+    .eq(1)
+    .find('td')
+    .eq(7)
+    .find('img')
+    .click()
+  cy.get('#grid_active_cell .k-picker').click()
+
   cy.get('@country').then((country) => {
+    cy.get('ul[role=listbox]')
+      .should('be.visible')
+      .get('li')
+      .should('have.length.above', 2)
     cy.contains('ul[role=listbox] li', country).click()
 
-    cy.get('ul[role=listbox]').should('not.exist')
+    cy.get('.k-loading-mask').should('not.exist')
+
     cy.get('tbody tr')
+      .should('have.length.above', 3)
       .eq(1)
       .find('td')
       .eq(7)
